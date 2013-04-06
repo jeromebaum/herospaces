2323368e9ec0da7e9c53965cc50550f95478e985
Jerome Baum <jerome@jeromebaum.com>
2013-04-06 08:35:36 +0200

    Fix upload and start work on keys

--- server.js
+++ server.js
@@ -3,17 +3,31 @@ var path = require('path');
 var fs = require('fs');
 var util = require('util');
 var url = require('url');
+var querystring = require('querystring');
 var mime = require('mime');
 var connect = require('connect');
 
 var port = process.env.PORT || 4000;
 var basePath = './';
+var optionsFile = 'magic.options';
 
 
 function handleGET (req, res, next) {
-    if (req.method !== 'GET') { next(); };
+    if (req.method !== 'GET') { next(); return; };
     var uri = url.parse(req.url).pathname;
     var filename = path.join(basePath, uri);
+    var basename = path.basename(uri);
+    if (basename === optionsFile) {
+        isAuthed(filename, req, function handleAuth (authed) {
+            if (!authed) {
+                res.writeHead(403);
+                res.end('403 No Thanks\n');
+            } else {
+                respondFile(filename, res);
+            };
+        });
+        return;
+    };
     fs.exists(filename, function respond (response) {
         if (!!response) {
             respondFile(filename, res);
@@ -25,9 +39,26 @@ function handleGET (req, res, next) {
 };
 
 function handlePUT (req, res, next) {
-    if (req.method !== 'PUT') { next(); };
+    if (req.method !== 'PUT') { next(); return; };
     var uri = url.parse(req.url).pathname;
-    var filename = path.join(basePath, req.uri);
+    var filename = path.join(basePath, uri);
+    req.pause();
+    isAuthed(filename, req, function handleAuth (authed) {
+        if (!authed) {
+            res.writeHead(403);
+            res.end('403 No Thanks\n');
+        } else {
+            handleFileUpload(req, res, filename);
+        };
+    });
+};
+
+function errorOut (req, res) {
+    res.writeHead(500);
+    res.end('500 Problem\n');
+};
+
+function handleFileUpload (req, res, filename, buffer) {
     var stream = fs.createWriteStream(filename);
     stream.on('error', function handlePUTError (error) {
         console.warn('Error creating WriteStream for file ' + filename);
@@ -35,45 +66,81 @@ function handlePUT (req, res, next) {
         res.writeHead(500);
         res.end('500 Create Failed\n');
     });
-    stream.on('close', function respond () {
+    req.resume();
+    req.on('end', function respond () {
         res.writeHead(201);
         res.end('201 Created\n');
     });
-    req.setEncoding('utf8');
-    req.on('data', function forwardData (data) {
-        stream.write(data);
-    });
-    req.on('end', function handleEndOfData () {
-        if (stream.writable) {
-            stream.end();
-        };
-    });
+    req.pipe(stream);
 };
 
 function respondFile (filename, res) {
     var theFilename = filename;
-    fs.stat(filename, function handleStat (stat) {
+    fs.stat(theFilename, function handleStat (stat) {
         if (stat && stat.isDirectory) {
             theFilename = path.join(filename, 'index.html');
         };
-        fs.readFile(filename, function handleFile (exists, file) {
-            if (!!exists) {
-                console.log(filename + ' does not exist');
+        fs.exists(theFilename, function handleStat (exists) {
+            if (!exists) {
                 res.writeHead(404);
                 res.end('404 Not Found\n');
             } else {
-                var type = mime.lookup(filename);
+                var type = mime.lookup(theFilename);
                 if (type === 'application/javascript') {
                     type = type + ';charset=utf-8';
                 };
                 res.writeHead(200, { 'Content-Type': type });
-                res.write(file, 'binary');
-                res.end();
+                fs.createReadStream(theFilename).pipe(res);
             };
         });
     });
 };
 
-var app = connect().use(handleGET).use(handlePUT);
+function isAuthed (filename, req, cb) {
+    var qs = url.parse(req.url).query;
+    var options = querystring.parse(qs);
+    var providedKey = options.key;
+    writeKeyFor(filename, function handleKey (writeKey) {
+        cb(writeKey === providedKey);
+    });
+};
+
+function findOptionsFile (filename, cb) {
+    var dirname = path.dirname(filename);
+    var candidate = path.join(dirname, optionsFile);
+    fs.exists(candidate, function handleStat (exists) {
+        if (exists) {
+            cb(candidate);
+        } else {
+            var oneLevelUp = path.resolve(filename, '..');
+            if (oneLevelUp === filename) {
+                cb();
+                return;
+            };
+            findOptionsFile(oneLevelUp, cb);
+        };
+    });
+};
+
+function optionsFor (filename, cb) {
+    findOptionsFile(filename, function (optionsFilename) {
+        if (!optionsFilename) {
+            cb();
+            return;
+        }
+        fs.readFile(optionsFilename, function handleOptionsFile (exists, file) {
+            var obj = JSON.parse(file);
+            cb(obj);
+        });
+    });
+};
+
+function writeKeyFor (filename, cb) {
+    optionsFor(filename, function (options) {
+        cb(options && options.writeKey);
+    });
+};
+
+var app = connect().use(handleGET).use(handlePUT).use(errorOut);
 
 http.createServer(app).listen(port);
